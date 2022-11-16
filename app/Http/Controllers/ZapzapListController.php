556707750a58c51c811f59019b9b702f8de36edc
Lourenco Werneck <lourencovw@gmail.com>
2022-11-16 02:55:53 +0000

    Initial commit

---
+++ app/Http/Controllers/ZapzapListController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreZapzapListRequest;
+use App\Http\Requests\UpdateZapzapListRequest;
+use App\Models\ZapzapList;
+
+class ZapzapListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreZapzapListRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreZapzapListRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ZapzapList  $zapzapList
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ZapzapList $zapzapList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ZapzapList  $zapzapList
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ZapzapList $zapzapList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateZapzapListRequest  $request
+     * @param  \App\Models\ZapzapList  $zapzapList
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateZapzapListRequest $request, ZapzapList $zapzapList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ZapzapList  $zapzapList
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ZapzapList $zapzapList)
+    {
+        //
+    }
+}
---
+++ app/Models/ZapzapList.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ZapzapList extends Model
+{
+    use HasFactory;
+}
